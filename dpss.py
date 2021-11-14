6564550936bbc452bd348f2259c21ebadf9fccdf
macaba <macaba@gmail.com>
2021-11-14 17:25:45 +0000

    Final version (for now)

--- dpss.py
+++ dpss.py
@@ -1,14 +1,36 @@
 import sys
 import json
 import scipy.signal
+import math
+
+# Input file parameters:
+
+# Samples: int
+# Window length.
+
+# SampleRate: int
+# ADC sample rate.
+
+# dF: float
+# Minimum distance between frequency peaks you expect to resolve
 
 from pathlib import Path
 python_file_path = Path(__file__)
 dir_path = python_file_path.parent
 input_file_path = dir_path / 'dpss_input.json'
+output_file_path = dir_path / 'dpss_output.bin'
 
 with open( input_file_path ) as data_file:   
 	input_args = json.loads( data_file.read() )
+
+Samples, SampleRate = [ int(input_args.get( key )) for key in [ 'Samples', 'SampleRate' ] ]
+dF = float(input_args.get( 'dF' ))	# Frequency resolution
+N = Samples/SampleRate				# Size of the data window in seconds
+NW = 4
+Kmax = math.floor((N * dF) - 1)
+
+# dpss parameters:
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.windows.dpss.html
 
 # M: int
 # Window length.
@@ -19,12 +41,15 @@ with open( input_file_path ) as data_file:
 
 # Kmax: int | None, optional
 # Number of DPSS windows to return (orders 0 through Kmax-1). If None (default), return only a single window of shape (M,) instead of an array of windows of shape (Kmax, M).
+# Kmax should be (M * dF) - 1
 
 # sym: bool, optional
 # When True (default), generates a symmetric window, for use in filter design. When False, generates a periodic window, for use in spectral analysis.
 
-M, Kmax = [ int(input_args.get( key )) for key in [ 'M', 'Kmax' ] ]
-NW = [ float(input_args.get( key )) for key in [ 'NW' ] ]
+win_dpss = scipy.signal.windows.dpss(Samples, NW, Kmax, False, 'subsample')
 
-win_dpss = scipy.signal.windows.dpss(M, NW, Kmax, False)
+output_file = open(output_file_path, 'wb')
+for window in win_dpss:
+	output_file.write(window)
 
+print("Done")
